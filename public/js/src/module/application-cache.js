ad56c8cd7301d9abdd85dc5e4fa0a1182d5e4af1
Martijn van de Rijdt <martijn@enketo.org>
2020-03-10 17:51:39 -0600

    fixed update check interval logic

--- public/js/src/module/application-cache.js
+++ public/js/src/module/application-cache.js
@@ -9,32 +9,36 @@ function init( survey ) {
 
     if ( 'serviceWorker' in navigator ) {
         window.addEventListener( 'load', function() {
-            navigator.serviceWorker.register( `${settings.basePath}/x/offline-app-worker.js` ).then( function( registration ) {
-                // Registration was successful
-                console.log( 'Offline application service worker registration successful with scope: ', registration.scope );
-                setInterval( () => registration.update, 60 * 60 * 1000 ); // DEBUG set to 1 minute to test updates during session
+            navigator.serviceWorker.register( `${settings.basePath}/x/offline-app-worker.js` )
+                .then( function( registration ) {
+                    // Registration was successful
+                    console.log( 'Offline application service worker registration successful with scope: ', registration.scope );
+                    setInterval( () => {
+                        console.log( 'Checking for offlince application cache service worker update' );
+                        registration.update();
+                    }, 60 * 60 * 1000 );
 
-                if ( registration.active ) {
-                    _reportOfflineLaunchCapable( true );
-                }
-                registration.addEventListener( 'updatefound', () => {
-                    const newWorker = registration.installing;
+                    if ( registration.active ) {
+                        _reportOfflineLaunchCapable( true );
+                    }
+                    registration.addEventListener( 'updatefound', () => {
+                        const newWorker = registration.installing;
 
-                    //console.log( 'new worker state', newWorker.state );
-                    newWorker.addEventListener( 'statechange', () => {
-                        //console.log( 'newWorker statechange event', newWorker.state, 'newWorker.active?', newWorker.active );
-                        if ( newWorker.state === 'activated' ) {
-                            console.log( 'New offline application service worker activated!' );
-                            document.dispatchEvent( events.ApplicationUpdated() );
-                        }
+                        newWorker.addEventListener( 'statechange', () => {
+
+                            if ( newWorker.state === 'activated' ) {
+                                console.log( 'New offline application service worker activated!' );
+                                document.dispatchEvent( events.ApplicationUpdated() );
+                            }
+
+                        } );
                     } );
-                } );
 
-            }, function( err ) {
-                // registration failed :(
-                console.error( 'Offline application service worker registration failed: ', err );
-                _reportOfflineLaunchCapable( true );
-            } );
+                }, function( err ) {
+                    // registration failed :(
+                    console.error( 'Offline application service worker registration failed: ', err );
+                    _reportOfflineLaunchCapable( true );
+                } );
         } );
     } else {
         console.error( 'Service workers not supported on this browser. This form cannot launch online' );
