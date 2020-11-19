22cc66c3ad2fbb8d29da3395caee22c5278aea04
LaimeJesus <jesus.laime@camba.coop>
2020-11-19 20:03:04 -0300

    Hotfix/#5 dockerfile does not work (#7)
    * fix Dockerfile using node image and start script
    
    * change default config using new dependencies
    
    * update dependencies
    
    * fix npm modules to specific versions
    
    * remove unnecesary code in start script
    
    * update favicon and icon by kobotoolbox ones

---
+++ setup/docker/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+source /etc/profile
+
+cd ${ENKETO_SRC_DIR}/
+
+# Create a config. file if necessary.
+python setup/docker/create_config.py
+
+grunt
+# Run Enketo via PM2 (without daemonizing, so logs are exposed
+#   e.g. via `docker logs enketoexpress_enketo_1`).
+exec pm2 start --no-daemon app.js -n enketo
